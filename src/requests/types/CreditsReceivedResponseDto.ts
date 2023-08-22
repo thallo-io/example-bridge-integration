fb77550b73159742af93fb766031c4c0050943c3
Derked <nazek8@gmail.com>
2023-08-22 10:42:49 -0400

    update example with tenant customer id

--- src/requests/types/CreditsReceivedResponseDto.ts
+++ src/requests/types/CreditsReceivedResponseDto.ts
@@ -7,4 +7,8 @@ export class CreditsReceivedResponseDto {
      * Registry's event ID
      */
     public external_id!: string
+    /**
+      The id of the project developer in Thallo's system. Represents the same as the sender_id in the request
+    */
+    public tenant_customer_id?: string
 }
