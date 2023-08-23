97cb5d043377f776247ccbbc3fc0ea646b9d999d
Derked <nazek8@gmail.com>
2023-08-23 10:11:08 -0400

    addressing comments

--- src/requests/types/CreditsReceivedResponseDto.ts
+++ src/requests/types/CreditsReceivedResponseDto.ts
@@ -8,7 +8,8 @@ export class CreditsReceivedResponseDto {
      */
     public external_id!: string
     /**
-      The id of the project developer in Thallo's system. Represents the same as the sender_id in the request
-    */
+     * The id of the project developer in Thallo's system. Represents the same as the sender_id in the request.
+     * This may not be used depending on the registry. It can be configured upon request.
+     */
     public tenant_customer_id?: string
 }
